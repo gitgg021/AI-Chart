35b02c3d30222179ff3159e3fc09e3a54a926d21
caoxuehan <caoxuehan@qq.com>
2024-01-15 01:46:24 +0800

    更新sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -20,19 +20,24 @@ create table if not exists user
     index idx_unionAccount (userAccount)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 图表信息表
-create table if not exists chart
+create table chart
 (
-    id         bigint auto_increment comment 'id' primary key,
-    goal       text null comment '分析目标',
-    chartData  text null comment '图表数据',
-    chartType  varchar(512) null comment '图表类型',
-    genChart   text null comment  '生成的图标数据',
-    genResult  text null comment  '生成的分析结论',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除'
-) comment '图表信息表' collate = utf8mb4_unicode_ci;
+    id          bigint auto_increment comment 'id'
+        primary key,
+    goal        text                               null comment '分析目标',
+    chartData   text                               null comment '图表数据',
+    chartType   varchar(128)                       null comment '图表类型',
+    genChart    text                               null comment '生成的图表数据',
+    genResult   text                               null comment '生成的分析结论',
+    userId      bigint                             null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
+    name        varchar(128)                       null comment '图表名称',
+    execMessage text                               null comment '执行信息',
+    chartState  int      default 0                 not null comment '生成状态：0:等待1:运行中2:失败3:成功'
+)
+    comment '图表信息表' collate = utf8mb4_unicode_ci;
 
 
 
