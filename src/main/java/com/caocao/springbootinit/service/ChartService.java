eeac9767a7d09c3d15d857fa2de661068558f7b7
caoxuehan <caoxuehan@qq.com>
2024-01-19 17:18:03 +0800

    chartService增加handleChartUpdateError方法

--- src/main/java/com/caocao/springbootinit/service/ChartService.java
+++ src/main/java/com/caocao/springbootinit/service/ChartService.java
@@ -2,12 +2,15 @@ package com.caocao.springbootinit.service;
 
 import com.caocao.springbootinit.model.entity.Chart;
 import com.baomidou.mybatisplus.extension.service.IService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
 * @author caocao
 * @description 针对表【chart(图表信息表)】的数据库操作Service
 * @createDate 2024-01-05 17:55:30
 */
-public interface ChartService extends IService<Chart> {
 
+public interface ChartService extends IService<Chart> {
+    //定义一个异常工具类
+    public void handleChartUpdateError(long chartId, String execMessage);
 }
--- src/main/java/com/caocao/springbootinit/service/impl/ChartServiceImpl.java
+++ src/main/java/com/caocao/springbootinit/service/impl/ChartServiceImpl.java
@@ -15,7 +15,19 @@ import org.springframework.stereotype.Service;
 public class ChartServiceImpl extends ServiceImpl<ChartMapper, Chart>
     implements ChartService{
 
-}
+    @Override
+    public void handleChartUpdateError(long chartId, String execMessage) {
+        Chart updateChartResult = new Chart();
+        updateChartResult.setId(chartId);
+        updateChartResult.setStatus("failed");
+        updateChartResult.setExecMessage(execMessage);
+        boolean updateResult = updateById(updateChartResult);
+        if (!updateResult) {
+            log.error("更新图表失败" + chartId + "," + execMessage);
+        }
+    }
+    }
+
 
 
 
